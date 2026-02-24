d4c763933e83aa292625262b7032453170a1ce65
jsasanam <jsasanam@ghx.com>
2026-02-24 18:49:56 +0530

    latest code

---
+++ src/main/java/exercise/bidashboardapi/BiDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package exercise.bidashboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiDashboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiDashboardApiApplication.class, args);
+    }
+
+}
